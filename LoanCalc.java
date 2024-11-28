1f169edffbb2b978601047749a9fbd7be1827238
Amit Herzberg <amitherzberg@Amits-MacBook-Air.local>
2024-11-28 15:55:27 +0200

    LoanCalc

--- LoanCalc.java
+++ LoanCalc.java
@@ -30,11 +30,11 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		while (n > 0) {
-			loan = (loan - payment) * (1 + rate / 100);
-			n--;
+		double endBalance = loan;
+		for(int i = 0; i < n; i++){
+			endBalance = (endBalance - payment) * (1 + rate / 100);
 		}
-		return loan;
+		return endBalance;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -43,9 +43,14 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		
-
-		return 0;
+		double payment = loan/n;
+		double endBalance = endBalance(loan, rate, n, payment);
+		while (endBalance > 0){
+			payment += epsilon; 
+			endBalance = endBalance(loan, rate, n, payment);
+			iterationCounter++;
+		}
+		return payment;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -54,7 +59,22 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		iterationCounter=0;
+		double L = loan / n;
+		double H = loan;
+		double g = (L+H)/2;
+		
+		while (H-L > epsilon){
+			iterationCounter++;
+			if (endBalance(loan, rate, n, g) * endBalance(loan, rate, n, L) > 0){
+				L = g;
+			}
+			else{
+				H = g;
+			}
+			g = (L+H)/2;
+		}
+		double payment = g;
+		return payment;
     }
 }
