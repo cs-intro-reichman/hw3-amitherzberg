2ece1075d5492f62a492784a434dccb1679f36c1
Amit Herzberg <amitherzberg@Amits-MacBook-Air.local>
2024-11-28 14:55:31 +0200

    Algebra.java

--- Algebra.java
+++ Algebra.java
@@ -9,11 +9,12 @@ public class Algebra {
 	    System.out.println(plus(2,3));   // 2 + 3
 	    System.out.println(minus(7,2));  // 7 - 2
    		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
+ 		System.out.println(times(3,-4));  // 3 * 4
    		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
    		System.out.println(pow(5,3));      // 5^3
    		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
+		System.out.println(pow(-3,3));      // 3^5
+   		System.out.println(div(25,7));   // 12 / 3    
    		System.out.println(div(5,5));    // 5 / 5  
    		System.out.println(div(25,7));   // 25 / 7
    		System.out.println(mod(25,7));   // 25 % 7
@@ -25,43 +26,170 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		while (x2 != 0) {
+			if (x2 < 0) {
+				x1--;
+				x2++;
+			} 
+			else {
+				x1++;
+				x2--;
+			}
+		} 
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x1 == 0 && x2 < 0) {
+				int x3 = 0;
+				while (x2 < 0) {
+					x3++;
+					x2++;
+				}
+				return x3;
+		} else {
+			while (x2 != 0) {
+				if (x2 < 0 && x1 > 0) {
+					x1--;
+					x2++;
+				} else if (x2 < 0 && x1 < 0) {
+					x1++;
+					x2++;
+				} else {
+					x1--;
+					x2--;
+				}
+			} 
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x3 = x1;
+		int opsitex1 = minus(0, x1);
+		int opsitex2 = minus(0, x2);
+		if (x1 == 0 || x2 == 0) {
+			return 0;
+		}
+		if (x2 < 0 && x1 < 0)  {
+			x3 = minus(0, x1);
+			for (int i = 1; i < opsitex2; i++) {
+				opsitex1 = plus(opsitex1,x3);
+			}
+			return opsitex1;
+		} else if (x2<0 && x1>0) {
+			for(int i=1;i<opsitex2;i++) {
+				x1 = plus(x1, x3);
+			}
+			return minus(0, x1);
+		} 
+		else {
+			for (int i = 1; i < x2; i++) {
+				x1 = plus(x1, x3);
+			}
+		}
+		return x1;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int m = x;
+		if (x == 0) {
+			return 0;
+		}
+		if (n==1) {
+			return x;
+		}
+		if (n == 0) {
+			return 1;
+		}
+		if (x < 0) {
+			int temp = x;
+			for (int i = 1; i < n; i++) {
+				x = times(x,temp);
+			} 
+			return x;
+		} else {
+			for (int i = 1; i < n; i++) {
+				x = times(x, m);
+			}
+		}
+		return x;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int counter = 0;
+		boolean isNegetive = false;
+		
+		if (x1 == 0) {
+			return 0;
+		}
+
+		if (x2 == 0) {
+			return 0;
+		} 
+
+		if (x1 == x2) {
+			return 1;
+		}
+
+		if ((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) {
+			isNegetive = true;
+
+			if (x1 < 0) {
+				x1 = times(x1, -1);
+			}
+
+			if (x2 < 0) {
+				x2 = times(x2, -1);
+			}
+		}
+
+		if (x1 < 0 && x2 < 0) {
+			if (x1 < 0) {
+				x1 = times(x1, -1);
+			}
+
+			if (x2 < 0) {
+				x2 = times(x2, -1);
+			}
+		}
+
+		while (x1 >= x2) {
+			x1 = minus(x1 , x2);
+			counter++;
+		}
+
+		if (isNegetive) {
+			return times(counter, -1);
+		} else {
+			return counter;
+		}
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int x3 = div(x1, x2);
+		int x4 = times(x2, x3);
+		int mod = minus(x1, x4);
+		return mod;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
+		for (int i = 1; i <= x; i++) {
+			if (times(i, i) == 0) {
+				return i;
+			} else {
+				if (times(plus(i, 1), plus(i, 1)) > x) {
+					return i;
+				}
+			}
+		} 
+		
 		return 0;
 	}	  	  
 }
--- LoanCalc.java
+++ LoanCalc.java
@@ -14,7 +14,9 @@ public class LoanCalc {
 		int n = Integer.parseInt(args[2]);
 		System.out.println("Loan = " + loan + ", interest rate = " + rate + "%, periods = " + n);
 
+
 		// Computes the periodical payment using brute force search
+		
 		System.out.print("\nPeriodical payment, using brute force: ");
 		System.out.println((int) bruteForceSolver(loan, rate, n, epsilon));
 		System.out.println("number of iterations: " + iterationCounter);
@@ -28,8 +30,11 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		while (n > 0) {
+			loan = (loan - payment) * (1 + rate / 100);
+			n--;
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,7 +43,8 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
+		
+
 		return 0;
     }
     
